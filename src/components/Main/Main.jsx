67e80399b4f93036c8bd7e8745e97d12c95a7d85
Ivan Lev <fanatos@mail.ru>
2024-01-26 12:06:49 +0500

    feat: add Techs component to Main

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -3,12 +3,14 @@ import './Main.css';
 import React from 'react';
 import Promo from '../Promo/Promo';
 import AboutProject from '../AboutProject/AboutProject';
+import Techs from '../Techs/Techs';
 
 export default function Main() {
   return (
     <main className="main">
       <Promo />
       <AboutProject />
+      <Techs />
     </main>
   );
 }
