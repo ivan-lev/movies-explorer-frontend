6ad28fcd058cfd9f9086e47d4091f149b9134d6f
Ivan Lev <fanatos@mail.ru>
2024-01-26 21:20:11 +0500

    feat: make footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,12 +1,26 @@
-import React from 'react';
-
 import './Footer.css';
+
+import React from 'react';
+import CurrentYear from '../CurrentYear/CurrentYear';
 
 export default function Footer() {
   return (
     <footer className="footer">
-      <p>some footer</p>
-      <p>will be here</p>
+      <p className="footer__info">Учебный проект Яндекс.Практикум х BeatFilm.</p>
+      <div className="footer__copyright">
+        <div className="footer__links">
+          <a className="footer__link" href="https://github.com/ivan-lev">
+            Github
+          </a>
+          <a className="footer__link" href="https://practicum.yandex.ru/">
+            Яндекс.Практикум
+          </a>
+        </div>
+        <span className="footer__current-year">
+          &copy;&nbsp;
+          <CurrentYear />
+        </span>
+      </div>
     </footer>
   );
 }
