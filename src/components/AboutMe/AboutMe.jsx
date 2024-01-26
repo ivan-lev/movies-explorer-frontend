4baab2348d73a1a64d6c472e8c55c1e8ccbd6188
Ivan Lev <fanatos@mail.ru>
2024-01-26 21:19:02 +0500

    feat: add AboutMe component

---
+++ src/components/AboutMe/AboutMe.jsx
@@ -0,0 +1,40 @@
+import './AboutMe.css';
+
+import React from 'react';
+
+import avatar from '../../images/avatar.jpg';
+
+export default function AboutMe() {
+  return (
+    <section className="about-me">
+      <h2 className="main__title">Студент</h2>
+      <div className="about-me__info">
+        <p className="about-me__name">Иван</p>
+        <p className="about-me__job-age">Фронтенд-разработчик, 36 лет</p>
+        <p className="about-me__description">
+          Я родился и живу в Екатеринбурге, в своё время закончил факультет Экономики и
+          предпринимательства в МГТУ. Я люблю слушать музыку, фотографировать, а ещё увлекаюсь
+          китайским чаем. Недавно начал кодить и увлёкся настолько, что прошёл курс по
+          веб-разработке.
+        </p>
+        <a href="https://github.com/ivan-lev" className="about-me__github-link">
+          Github
+        </a>
+        <div className="about-me__avatar-wrapper"></div>
+        <img className="about-me__avatar" src={avatar} alt="author"></img>
+      </div>
+      <p className="about-me__portfolio">Портфолио</p>
+      <a className="about-me__link" href="https://ivan-lev.github.io/how-to-learn/">
+        Статичный сайт
+      </a>
+      <div className="about-me__separator"></div>
+      <a className="about-me__link" href="https://ivan-lev.github.io/russian-travel/">
+        Адаптивный сайт
+      </a>
+      <div className="about-me__separator"></div>
+      <a className="about-me__link" href="https://quietplace.nomoredomainsmonster.ru/">
+        Одностраничное приложение
+      </a>
+    </section>
+  );
+}
