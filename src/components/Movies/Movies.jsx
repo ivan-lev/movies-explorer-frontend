ac2350ad5109c0c3aa6788cbce0a0b0a9fbe30df
Ivan Lev <fanatos@mail.ru>
2024-01-29 23:55:47 +0500

    feat: add Button component to Movies

--- src/components/Movies/Movies.jsx
+++ src/components/Movies/Movies.jsx
@@ -2,6 +2,7 @@ import './Movies.css';
 
 import React from 'react';
 import MoviesCardList from '../MoviesCardList/MoviesCardList';
+import Button from '../Button/Button';
 
 import { movies } from '../../variables/movieList';
 
@@ -9,6 +10,7 @@ export default function Movies() {
   return (
     <section className="main__section movies">
       <MoviesCardList list={movies} />
+      <Button type="bordered" text="Ещё" onClick={() => console.log('Expand page!')} />
     </section>
   );
 }
