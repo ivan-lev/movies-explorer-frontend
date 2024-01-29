3e9b6cd121c26a5d18bd477bb019244aeeea3f36
Ivan Lev <fanatos@mail.ru>
2024-01-29 20:35:25 +0500

    feat: add movieList for storing temporary data for rendering MovieCards components

---
+++ src/variables/movieList.js
@@ -0,0 +1,86 @@
+export const movies = [
+  {
+    title: '33 слова о дизайне',
+    duration: 77,
+    cover: '01.jpg',
+    preview: '01_preview.jpg',
+    isSaved: false
+  },
+  {
+    title: 'Киноальманах «100 лет дизайна»',
+    duration: 78,
+    cover: '02.jpg',
+    preview: '02_preview.jpg',
+    isSaved: true
+  },
+  {
+    title: 'В погоне за Бенкси',
+    duration: 30,
+    cover: '03.jpg',
+    preview: '03_preview.jpg',
+    isSaved: false
+  },
+  {
+    title: 'Баския: Взрыв реальности',
+    duration: 80,
+    cover: '04.jpg',
+    preview: '04_preview.jpg',
+    isSaved: false
+  },
+  {
+    title: 'Бег это свобода',
+    duration: 51,
+    cover: '05.jpg',
+    preview: '05_preview.jpg',
+    isSaved: false
+  },
+  {
+    title: 'Книготорговцы',
+    duration: 72,
+    cover: '06.jpg',
+    preview: '06_preview.jpg',
+    isSaved: true
+  },
+  {
+    title: 'Когда я думаю о Германии ночью',
+    duration: 89,
+    cover: '07.jpg',
+    preview: '07_preview.jpg',
+    isSaved: false
+  },
+  {
+    title: 'Gimme Danger: История Игги и The Stooges',
+    duration: 67,
+    cover: '08.jpg',
+    preview: '08_preview.jpg',
+    isSaved: false
+  },
+  {
+    title: 'Дженис: Маленькая девочка грустит',
+    duration: 119,
+    cover: '09.jpg',
+    preview: '09_preview.jpg',
+    isSaved: false
+  },
+  {
+    title: 'Соберись перед прыжком',
+    duration: 47,
+    cover: '10.png',
+    preview: '10_preview.jpg',
+    isSaved: false
+  },
+  {
+    title: 'Пи Джей Харви: A dog called money',
+    duration: 96,
+    cover: '11.jpg',
+    preview: '11_preview.jpg',
+    isSaved: false
+  },
+  {
+    title: 'По волнам: Искусство звука в кино',
+    duration: 129,
+    cover: '12.jpg',
+    preview: '12_preview.jpg',
+    isSaved: false
+  }
+];
