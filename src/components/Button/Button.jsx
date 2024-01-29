04681ffbf5c94a3b92fc3eee68d0474768fe7685
Ivan Lev <fanatos@mail.ru>
2024-01-29 23:54:06 +0500

    fix/style: fix bem naming and add new type of button

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -3,7 +3,7 @@ import './Button.css';
 
 export default function Button({ type, text, onClick }) {
   return (
-    <button className={`button button__${type}`} onClick={onClick}>
+    <button className={`button button_${type}`} onClick={onClick}>
       {text}
     </button>
   );
