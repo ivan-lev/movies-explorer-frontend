f25d6119e98fac90ed0c69a8f896d205f64d817e
Ivan Lev <fanatos@mail.ru>
2024-01-29 11:29:48 +0500

    feat: add initial routing in App

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,18 +1,27 @@
 import './App.css';
 
 import { useState } from 'react';
+import { Routes, Route } from 'react-router-dom';
 
 import Header from '../Header/Header';
 import Main from '../Main/Main';
 import Footer from '../Footer/Footer';
+import Navigation from '../Navigation/Navigation';
+import Logo from '../Logo/Logo';
 
 function App() {
-  const [isLoggedIn, setIsLoggedIn] = useState(true);
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
 
   return (
     <div className="App">
-      <Header isLoggedIn={isLoggedIn} />
-      <Main />
+      <Header>
+        <Logo />
+        <Navigation isLoggedIn={isLoggedIn} />
+      </Header>
+      <Routes>
+        <Route path="/" element={<Main />} />
+      </Routes>
+
       <Footer />
     </div>
   );
