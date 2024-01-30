fa60613c2418a53192feff97e24172e2d23e8511
Ivan Lev <fanatos@mail.ru>
2024-01-30 14:53:04 +0500

    fead: add SavedMovies component

--- src/components/SavedMovies/SavedMovies.jsx
+++ src/components/SavedMovies/SavedMovies.jsx
@@ -1,11 +1,25 @@
 import './SavedMovies.css';
 
 import React from 'react';
+import { useMatch } from 'react-router-dom';
+import MoviesCardList from '../MoviesCardList/MoviesCardList';
+import Button from '../Button/Button';
+
+import { movies } from '../../variables/movieList';
 
 export default function SavedMovies() {
+  const pathMatchedSavedMovies = useMatch('/saved-movies');
+
+  const savedMoviesList = movies.filter(movie => {
+    return movie.isSaved;
+  });
+
   return (
-    <section className="saved-movies">
-      <div> Сохранённые фильмы</div>
+    <section className="main__section saved-movies">
+      <MoviesCardList list={savedMoviesList} />
+      {!pathMatchedSavedMovies && (
+        <Button type="bordered" text="Ещё" onClick={() => console.log('Expand page!')} />
+      )}
     </section>
   );
 }
