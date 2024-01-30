af2f8d43eb0f6ec2bb9d5a9afcc4e4e88e74102c
Ivan Lev <fanatos@mail.ru>
2024-01-30 18:10:32 +0500

    feat: add Profile component

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,10 +1,35 @@
 import './Profile.css';
+
 import React from 'react';
+import { useState } from 'react';
+
+import Button from '../Button/Button';
 
 export default function Profile() {
+  const [userName, setUserName] = useState('Иван');
+  const [email, setEmail] = useState('ivan@mail.com');
+
   return (
     <section className="profile">
-      <div>Фильмы</div>
+      <span className="profile__greeting">Привет, {userName}!</span>
+      <div className="profile__data">
+        <div className="profile__row">
+          <span className="profile__field">Имя</span>
+          <span className="profile__field">{userName}</span>
+        </div>
+        <div className="profile__divider"></div>
+        <div className="profile__row">
+          <span className="profile__field">E-mail</span>
+          <span className="profile__field">{email}</span>
+        </div>
+      </div>
+      <div className="profile__wrapper">
+        <Button type="transparent button_bigger-font" text="Редактировать"></Button>
+        <Button
+          type="transparent button_bigger-font button_text-crimson"
+          text="Выйти из аккаутна"
+        ></Button>
+      </div>
     </section>
   );
 }
