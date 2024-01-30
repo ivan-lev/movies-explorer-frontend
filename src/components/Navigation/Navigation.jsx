71c2a40a58b3a5d45c699cada8f02a203f5169b3
Ivan Lev <fanatos@mail.ru>
2024-01-30 18:40:51 +0500

    feat: refactor header components for better layout

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -8,36 +8,26 @@ export default function Navigation({ isLoggedIn }) {
   const navigate = useNavigate();
 
   return (
-    <span className="navigation">
-      {isLoggedIn ? (
-        <>
-          <span className="navigation__films">
-            <NavLink
-              className={({ isActive }) =>
-                `navigation__navlink ${isActive ? 'navigation__navlink_active' : ''}`
-              }
-              //className="navigation__navlink"
-              to="/movies"
-            >
-              Фильмы
-            </NavLink>
+    <span>
+      {isLoggedIn && (
+        <span className="navigation">
+          <NavLink
+            className={({ isActive }) =>
+              `navigation__navlink ${isActive ? 'navigation__navlink_active' : ''}`
+            }
+            to="/movies"
+          >
+            Фильмы
+          </NavLink>
 
-            <NavLink
-              className={({ isActive }) =>
-                `navigation__navlink ${isActive ? 'navigation__navlink_active' : ''}`
-              }
-              to="/saved-movies"
-            >
-              Сохранённые фильмы
-            </NavLink>
-          </span>
-
-          <Button type="gray" text="Аккаунт" onClick={() => navigate('/profile')} />
-        </>
-      ) : (
-        <span className="navigation__auth">
-          <Button type="transparent" text="Регистрация" onClick={() => navigate('/signup')} />
-          <Button type="green" text="Войти" onClick={() => navigate('/signin')} />
+          <NavLink
+            className={({ isActive }) =>
+              `navigation__navlink ${isActive ? 'navigation__navlink_active' : ''}`
+            }
+            to="/saved-movies"
+          >
+            Сохранённые фильмы
+          </NavLink>
         </span>
       )}
     </span>
