f0e8019e29df699a21ff0b28ea7f108ed49f0102
Ivan Lev <fanatos@mail.ru>
2024-01-31 00:57:20 +0500

    feat: add initial Register component

--- src/components/Register/Register.jsx
+++ src/components/Register/Register.jsx
@@ -1,10 +1,104 @@
 import './Register.css';
-import React from 'react';
 
-export default function Register() {
+import React from 'react';
+import { useState } from 'react';
+import { Link } from 'react-router-dom';
+
+import Logo from '../Logo/Logo';
+import Button from '../Button/Button';
+
+export default function Register({ onSubmit }) {
+  const [formValues, setFormValues] = useState({
+    email: '',
+    password: '',
+    name: ''
+  });
+
+  const handleChange = event => {
+    const { name, value } = event.target;
+
+    setFormValues({
+      ...formValues,
+      [name]: value
+    });
+  };
+
+  const handleSubmit = event => {
+    event.preventDefault();
+    console.log(formValues.name);
+    const { name, email, password } = formValues;
+    console.log(name);
+    alert('Отправляем форму с параметрами: ' + name + ', ' + email + ', ' + password);
+  };
+
   return (
     <section className="register">
-      <div>Фильмы</div>
+      <Logo />
+      <p className="register__title">Добро пожаловать!</p>
+      <form
+        id="register__form"
+        name="register__form"
+        className="register__form"
+        onSubmit={handleSubmit}
+      >
+        <fieldset className="register__fieldset">
+          <label className="register__input-label">
+            Имя
+            <input
+              type="text"
+              autoComplete="on"
+              placeholder=""
+              id="name"
+              name="name"
+              className="register__input"
+              required
+              onChange={handleChange}
+            />
+          </label>
+          <span className="register__input-error">Ji</span>
+        </fieldset>
+
+        <fieldset className="register__fieldset">
+          <label className="register__input-label">
+            E-mail
+            <input
+              type="email"
+              autoComplete="on"
+              placeholder=""
+              id="email"
+              name="email"
+              className="register__input"
+              required
+              onChange={handleChange}
+            />
+          </label>
+          <span className="register__input-error">dgd</span>
+        </fieldset>
+
+        <fieldset className="register__fieldset">
+          <label className="register__input-label">
+            Пароль
+            <input
+              type="password"
+              autoComplete="on"
+              placeholder=""
+              id="password"
+              name="password"
+              className="register__input"
+              required
+              onChange={handleChange}
+            />
+          </label>
+          <span className="register__input-error">ddg</span>
+        </fieldset>
+        <Button type="blue" text="Зарегистрироваться" />
+      </form>
+      <span className="register__already-registered-wrapper">
+        <span className="register__already-registered-text">Уже зарегистрированы? </span>
+        <Link className="register__login-link" to="/signin">
+          Войти
+        </Link>
+      </span>
     </section>
   );
 }
