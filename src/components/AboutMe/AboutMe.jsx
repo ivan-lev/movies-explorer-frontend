f9c3ba9a464452d5db30dd9fece78c5176a23306
Ivan Lev <fanatos@mail.ru>
2024-02-01 19:50:58 +0500

    style: add responsive styles fro middle screens

--- src/components/AboutMe/AboutMe.jsx
+++ src/components/AboutMe/AboutMe.jsx
@@ -20,7 +20,6 @@ export default function AboutMe() {
         <a href="https://github.com/ivan-lev" className="about-me__github-link">
           Github
         </a>
-        <div className="about-me__avatar-wrapper"></div>
         <img className="about-me__avatar" src={avatar} alt="author"></img>
       </div>
       <p className="about-me__portfolio">Портфолио</p>
