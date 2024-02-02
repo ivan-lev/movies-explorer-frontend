97d4f58f6a6d89579c547cb7d0baf1bd60d4810e
Ivan Lev <fanatos@mail.ru>
2024-02-03 00:37:30 +0500

    fix: fast fix of navigation buttons placing

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -1,6 +1,6 @@
 import './Navigation.css';
 
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { NavLink, useNavigate } from 'react-router-dom';
 
 import Button from '../Button/Button';
@@ -8,47 +8,112 @@ import MobileMenu from '../MobileMenu/MobileMenu';
 
 export default function Navigation({ isLoggedIn }) {
   const navigate = useNavigate();
-  const [isMenuOpened, setIsMenuOpened] = useState(false);
 
+  // if change isMenuOpened to 'true', then make overflow hidden for document
+  const [isMenuOpened, setIsMenuOpened] = useState(false);
   const toggleOpenMenu = () => {
-    document.body.style.overflow = `${isMenuOpened ? 'hidden' : ''}`;
     setIsMenuOpened(!isMenuOpened);
   };
+  useEffect(() => {
+    document.body.style.overflow = `${isMenuOpened ? 'hidden' : ''}`;
+  }, [isMenuOpened]);
 
-  return isLoggedIn ? (
+  return (
     <>
       {isMenuOpened && <MobileMenu onClose={toggleOpenMenu} />}
-      <Button type="burger" onClick={toggleOpenMenu}></Button>
-      <nav className="navigation__movies">
-        <NavLink
-          className={({ isActive }) =>
-            `navigation__navlink ${isActive ? 'navigation__navlink_active' : ''}`
-          }
-          to="/movies"
-        >
-          Фильмы
-        </NavLink>
+      <div className="MOVIES_DIV">
+        {isLoggedIn ? (
+          <nav className="navigation__movies">
+            <NavLink
+              className={({ isActive }) =>
+                `navigation__navlink ${isActive ? 'navigation__navlink_active' : ''}`
+              }
+              to="/movies"
+            >
+              Фильмы
+            </NavLink>
 
-        <NavLink
-          className={({ isActive }) =>
-            `navigation__navlink ${isActive ? 'navigation__navlink_active' : ''}`
-          }
-          to="/saved-movies"
-        >
-          Сохранённые фильмы
-        </NavLink>
-      </nav>
-      <div className="navigation__user-buttons">
-        <Button type="gray" text="Аккаунт" onClick={() => navigate('/profile')} />
+            <NavLink
+              className={({ isActive }) =>
+                `navigation__navlink ${isActive ? 'navigation__navlink_active' : ''}`
+              }
+              to="/saved-movies"
+            >
+              Сохранённые фильмы
+            </NavLink>
+          </nav>
+        ) : (
+          <div></div>
+        )}
       </div>
-    </>
-  ) : (
-    <>
-      <div></div>
-      <div className="navigation__auth-buttons">
-        <Button type="transparent" text="Регистрация" onClick={() => navigate('/signup')} />
-        <Button type="green" text="Войти" onClick={() => navigate('/signin')} />
+      <div className="USER_DIV">
+        {isLoggedIn ? (
+          <>
+            <div className="navigation__user-button">
+              <Button type="gray" text="Аккаунт" onClick={() => navigate('/profile')} />
+            </div>
+            <div className="navigation__mobile-menu-button">
+              <Button type="burger" onClick={toggleOpenMenu}></Button>
+            </div>
+          </>
+        ) : (
+          <div className="navigation__auth-buttons">
+            <Button type="transparent" text="Регистрация" onClick={() => navigate('/signup')} />
+            <Button type="green" text="Войти" onClick={() => navigate('/signin')} />
+          </div>
+        )}
       </div>
     </>
   );
+
+  // return isLoggedIn ? (
+  //   <>
+  //     {isMenuOpened && <MobileMenu onClose={toggleOpenMenu} />}
+  //     <Button type="burger" onClick={toggleOpenMenu}></Button>
+  //
+  //
+  //   </>
+  // ) : (
+  //   <>
+  //     <div></div>
+  //
+  //   </>
+  // );
+
+  // return isLoggedIn ? (
+  //   <>
+  //     {isMenuOpened && <MobileMenu onClose={toggleOpenMenu} />}
+  //     <Button type="burger" onClick={toggleOpenMenu}></Button>
+  //     <nav className="navigation__movies">
+  //       <NavLink
+  //         className={({ isActive }) =>
+  //           `navigation__navlink ${isActive ? 'navigation__navlink_active' : ''}`
+  //         }
+  //         to="/movies"
+  //       >
+  //         Фильмы
+  //       </NavLink>
+
+  //       <NavLink
+  //         className={({ isActive }) =>
+  //           `navigation__navlink ${isActive ? 'navigation__navlink_active' : ''}`
+  //         }
+  //         to="/saved-movies"
+  //       >
+  //         Сохранённые фильмы
+  //       </NavLink>
+  //     </nav>
+  //     <div className="navigation__user-buttons">
+  //       <Button type="gray" text="Аккаунт" onClick={() => navigate('/profile')} />
+  //     </div>
+  //   </>
+  // ) : (
+  //   <>
+  //     <div></div>
+  //     <div className="navigation__auth-buttons">
+  //       <Button type="transparent" text="Регистрация" onClick={() => navigate('/signup')} />
+  //       <Button type="green" text="Войти" onClick={() => navigate('/signin')} />
+  //     </div>
+  //   </>
+  // );
 }
