478107a55f705609e1322d206d99709020126b29
Ivan Lev <fanatos@mail.ru>
2024-02-02 16:11:57 +0500

    style: delete unnecessary commented code

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -8,48 +8,6 @@ import Button from '../Button/Button';
 export default function Navigation({ isLoggedIn }) {
   const navigate = useNavigate();
 
-  //   return (
-  //     <>
-  //       <nav className="navigation__movies">
-  //         {isLoggedIn ? (
-  //           <>
-  //             <NavLink
-  //               className={({ isActive }) =>
-  //                 `navigation__navlink ${isActive ? 'navigation__navlink_active' : ''}`
-  //               }
-  //               to="/movies"
-  //             >
-  //               Фильмы
-  //             </NavLink>
-
-  //             <NavLink
-  //               className={({ isActive }) =>
-  //                 `navigation__navlink ${isActive ? 'navigation__navlink_active' : ''}`
-  //               }
-  //               to="/saved-movies"
-  //             >
-  //               Сохранённые фильмы
-  //             </NavLink>
-  //           </>
-  //         ) : (
-  //           <></>
-  //         )}
-  //       </nav>
-
-  //       <span className="navigation__user-buttons">
-  //         {isLoggedIn ? (
-  //           <Button type="gray" text="Аккаунт" onClick={() => navigate('/profile')} />
-  //         ) : (
-  //           <span className="navigation__auth-buttons">
-  //             <Button type="transparent" text="Регистрация" onClick={() => navigate('/signup')} />
-  //             <Button type="green" text="Войти" onClick={() => navigate('/signin')} />
-  //           </span>
-  //         )}
-  //       </span>
-  //     </>
-  //   );
-  // }
-
   return isLoggedIn ? (
     <>
       <nav className="navigation__movies">
