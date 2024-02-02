84ebc588a75fe61141b12eea91be8b5526a6f318
Ivan Lev <fanatos@mail.ru>
2024-02-02 22:27:11 +0500

    feat: add mobile menu

--- src/components/MobileMenu/MobileMenu.jsx
+++ src/components/MobileMenu/MobileMenu.jsx
@@ -1,11 +1,45 @@
 import './MobileMenu.css';
 
 import React from 'react';
+import { NavLink, useNavigate } from 'react-router-dom';
+
+import Button from '../Button/Button';
 
 export default function MobileMenu({ onClose }) {
+  const navigate = useNavigate();
   return (
     <div className="mobile-menu">
-      <button onClick={onClose}>X</button>
+      <div className="mobile-menu__overlay"></div>
+      <div className="mobile-menu__content">
+        <Button type="cross" onClick={onClose} />
+        <nav className="mobile-menu__nav-links">
+          <NavLink
+            to="/"
+            className={({ isActive }) =>
+              `mobile-menu__link ${isActive && 'mobile-menu__link_active'}`
+            }
+          >
+            Главная
+          </NavLink>
+          <NavLink
+            to="/movies"
+            className={({ isActive }) =>
+              `mobile-menu__link ${isActive && 'mobile-menu__link_active'}`
+            }
+          >
+            Фильмы
+          </NavLink>
+          <NavLink
+            to="/saved-movies"
+            className={({ isActive }) =>
+              `mobile-menu__link ${isActive && 'mobile-menu__link_active'}`
+            }
+          >
+            Сохранённые фильмы
+          </NavLink>
+        </nav>
+        <Button type="gray" text="Аккаунт" onClick={() => navigate('/profile')} />
+      </div>
     </div>
   );
 }
