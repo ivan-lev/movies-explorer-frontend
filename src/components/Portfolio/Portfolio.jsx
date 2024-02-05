715764bd9bf7db328ea7749b04cf9b15e016a43b
Ivan Lev <fanatos@mail.ru>
2024-02-05 16:17:04 +0500

    style: wrap links in ul-li tags for better semantics and update styles

--- src/components/Portfolio/Portfolio.jsx
+++ src/components/Portfolio/Portfolio.jsx
@@ -6,17 +6,38 @@ export default function Portfolio() {
   return (
     <section className="main__section portfolio" id="student">
       <h2 className="portfolio__title">Портфолио</h2>
-      <a className="portfolio__link" href="https://ivan-lev.github.io/how-to-learn/">
-        Статичный сайт
-      </a>
-      <div className="portfolio__separator"></div>
-      <a className="portfolio__link" href="https://ivan-lev.github.io/russian-travel/">
-        Адаптивный сайт
-      </a>
-      <div className="portfolio__separator"></div>
-      <a className="portfolio__link" href="https://quietplace.nomoredomainsmonster.ru/">
-        Одностраничное приложение
-      </a>
+      <ul className="portfolio__list">
+        <li className="portfolio__list-item">
+          <a
+            className="portfolio__link"
+            target="_blank"
+            rel="noreferrer"
+            href="https://ivan-lev.github.io/how-to-learn/"
+          >
+            Статичный сайт
+          </a>
+        </li>
+        <li className="portfolio__list-item">
+          <a
+            className="portfolio__link"
+            target="_blank"
+            rel="noreferrer"
+            href="https://ivan-lev.github.io/russian-travel/"
+          >
+            Адаптивный сайт
+          </a>
+        </li>
+        <li className="portfolio__list-item">
+          <a
+            className="portfolio__link"
+            target="_blank"
+            rel="noreferrer"
+            href="https://quietplace.nomoredomainsmonster.ru/"
+          >
+            Одностраничное приложение
+          </a>
+        </li>
+      </ul>
     </section>
   );
 }
