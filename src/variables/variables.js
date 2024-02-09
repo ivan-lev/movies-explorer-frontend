2333a41f6129ae447ef02ec4bc6586d9ea5f880d
Ivan Lev <fanatos@mail.ru>
2024-02-09 14:21:30 +0500

    level-3: initial commit

--- src/variables/variables.js
+++ src/variables/variables.js
@@ -2,3 +2,7 @@ export const emailRegExp =
   /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 
 export const nameRegexp = /^[a-zа-я ,.'-]+$/i;
+
+export const movieListUrl = 'https://api.nomoreparties.co/beatfilm-movies';
+
+export const baseUrl = 'https://moviesearch.nomoredomainsmonster.ru/';
