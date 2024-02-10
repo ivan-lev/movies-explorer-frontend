744508584293e2e670a9c2992cbc6cec0fa5e605
Ivan Lev <fanatos@mail.ru>
2024-02-10 18:21:18 +0500

    refactor/style: update variables with new regexps and styles

--- src/variables/variables.js
+++ src/variables/variables.js
@@ -1,7 +1,7 @@
 export const emailRegExp =
   /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 
-export const nameRegexp = /^[a-zA-Zа-яА-Я\ \-]+$/i;
+export const nameRegexp = /^[a-zA-Zа-яА-Я -]+$/i;
 
 export const movieListUrl = 'https://api.nomoreparties.co/beatfilm-movies';
 export const movieImageUrl = 'https://api.nomoreparties.co';
