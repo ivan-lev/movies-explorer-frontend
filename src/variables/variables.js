8790bd203a40507b04f0b5a459b326f0ba1c226d
Ivan Lev <fanatos@mail.ru>
2024-02-10 13:12:05 +0500

    feat: update name reg exp to accept hyphens

--- src/variables/variables.js
+++ src/variables/variables.js
@@ -1,11 +1,11 @@
 export const emailRegExp =
   /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 
-export const nameRegexp = /^[a-zа-я ,.'-]+$/i;
+export const nameRegexp = /^[a-zA-Zа-яА-Я\ \-]+$/i;
 
 export const movieListUrl = 'https://api.nomoreparties.co/beatfilm-movies';
 export const movieImageUrl = 'https://api.nomoreparties.co';
 
-export const baseUrl = 'https://moviesearch.nomoredomainsmonster.ru/';
+export const baseUrl = 'https://api.moviesearch.nomoredomainsmonster.ru';
 
 export const shortMeterDuration = 40;
