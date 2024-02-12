5e2f63339e462263e94c53b8b965f1b15d4688e8
Ivan Lev <fanatos@mail.ru>
2024-02-12 14:34:27 +0500

    feat: update validators to return objects with validity states and error messages

--- src/utils/formValidator.js
+++ src/utils/formValidator.js
@@ -3,117 +3,42 @@ import { errorMessages } from '../variables/errorMessages';
 
 const nameValidator = name => {
   if (!name) {
-    return 'Необходимо заполнить имя';
+    return { isValid: false, errorMessage: 'Необходимо заполнить имя' };
   }
 
   if (!name.match(nameRegexp)) {
-    return 'Поле имя содержит недопустимые символы';
+    return { isValid: false, errorMessage: 'Поле имя содержит недопустимые символы' };
   }
 
   if (name.length < 2) {
-    return 'Имя слишком короткое';
+    return { isValid: false, errorMessage: 'Имя слишком короткое' };
   }
 
   if (!name.length > 30) {
-    return 'Имя слишком длинное';
+    return { isValid: false, errorMessage: 'Имя слишком длинное' };
   }
 
-  return '';
+  return { isValid: true, errorMessage: '' };
 };
 
 const emailValidator = email => {
   if (!email) {
-    return 'Необходимо заполнить почту';
+    return { isValid: false, errorMessage: 'Необходимо заполнить почту' };
   }
 
   if (!email.match(emailRegExp)) {
-    return 'Почта заполнена неправильно';
+    return { isValid: false, errorMessage: 'Почта заполнена неправильно' };
   }
 
-  return '';
+  return { isValid: true, errorMessage: '' };
 };
 
 const passwordValidator = password => {
   if (!password) {
-    return 'Необходимо заполнить поле с паролем';
+    return { isValid: false, errorMessage: 'Необходимо заполнить поле с паролем' };
   }
 
-  return '';
-};
-
-export const validateProfileUpdate = (name, email, setMessage) => {
-  const validateName = nameValidator(name);
-  const validateEmail = emailValidator(email);
-
-  if (!name || !email) {
-    setMessage('Необходимо заполнить все поля');
-    return false;
-  }
-
-  if (validateName) {
-    setMessage(validateName);
-    return false;
-  }
-
-  if (validateEmail) {
-    setMessage(validateEmail);
-    return false;
-  }
-
-  setMessage('');
-  return true;
-};
-
-export const validateLogin = (email, password, setMessage) => {
-  const validateEmail = emailValidator(email);
-  const validatePassword = passwordValidator(password);
-
-  if (!email && !password) {
-    setMessage('Необходимо заполнить все поля');
-    return false;
-  }
-
-  if (validateEmail) {
-    setMessage(validateEmail);
-    return false;
-  }
-
-  if (validatePassword) {
-    setMessage(validatePassword);
-    return false;
-  }
-
-  setMessage('');
-  return true;
-};
-
-export const validateRegistration = (name, email, password, setMessage) => {
-  const validateName = nameValidator(name);
-  const validateEmail = emailValidator(email);
-  const validatePassword = passwordValidator(password);
-
-  if (!name && !email && !password) {
-    setMessage('Необходимо заполнить все поля');
-    return false;
-  }
-
-  if (validateName) {
-    setMessage(validateName);
-    return false;
-  }
-
-  if (validateEmail) {
-    setMessage(validateEmail);
-    return false;
-  }
-
-  if (validatePassword) {
-    setMessage(validatePassword);
-    return false;
-  }
-
-  setMessage('');
-  return true;
+  return { isValid: true, errorMessage: '' };
 };
 
 export const validateSearch = (value, setMessage) => {
@@ -126,4 +51,83 @@ export const validateSearch = (value, setMessage) => {
   return true;
 };
 
-export const validators = { nameValidator, emailValidator, passwordValidator };
+// export const validateProfileUpdate = (name, email, setMessage) => {
+//   const validateName = nameValidator(name);
+//   const validateEmail = emailValidator(email);
+
+//   if (!name || !email) {
+//     setMessage('Необходимо заполнить все поля');
+//     return false;
+//   }
+
+//   if (validateName) {
+//     setMessage(validateName);
+//     return false;
+//   }
+
+//   if (validateEmail) {
+//     setMessage(validateEmail);
+//     return false;
+//   }
+
+//   setMessage('');
+//   return true;
+// };
+
+// export const validateLogin = (email, password, setMessage) => {
+//   const validateEmail = emailValidator(email);
+//   const validatePassword = passwordValidator(password);
+
+//   if (!email && !password) {
+//     setMessage('Необходимо заполнить все поля');
+//     return false;
+//   }
+
+//   if (validateEmail) {
+//     setMessage(validateEmail);
+//     return false;
+//   }
+
+//   if (validatePassword) {
+//     setMessage(validatePassword);
+//     return false;
+//   }
+
+//   setMessage('');
+//   return true;
+// };
+
+// export const validateRegistration = (name, email, password, setMessage) => {
+//   const validateName = nameValidator(name);
+//   const validateEmail = emailValidator(email);
+//   const validatePassword = passwordValidator(password);
+
+//   if (!name && !email && !password) {
+//     setMessage('Необходимо заполнить все поля');
+//     return false;
+//   }
+
+//   if (validateName) {
+//     setMessage(validateName);
+//     return false;
+//   }
+
+//   if (validateEmail) {
+//     setMessage(validateEmail);
+//     return false;
+//   }
+
+//   if (validatePassword) {
+//     setMessage(validatePassword);
+//     return false;
+//   }
+
+//   setMessage('');
+//   return true;
+// };
+
+export const validators = {
+  nameValidator,
+  emailValidator,
+  passwordValidator
+};
