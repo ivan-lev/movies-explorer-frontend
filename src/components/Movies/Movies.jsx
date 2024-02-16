0ac53769968b3b159e6559efab69d04e1b7b484b
Ivan Lev <fanatos@mail.ru>
2024-02-16 18:42:01 +0500

    refactor: add search logic in one function

--- src/components/Movies/Movies.jsx
+++ src/components/Movies/Movies.jsx
@@ -11,55 +11,20 @@ import { searchMovies } from '../../utils/utils';
 
 import { ERROR_MESSAGES } from '../../variables/errorMessages';
 
-export default function Movies({ moviesList, savedMovies }) {
+export default function Movies({ savedMovies }) {
   const [searchQuery, setSearchQuery] = useLocalStorageState('searchQuery', '');
   const [allMovies, setAllMovies] = useState([]);
   const [searchResults, setSearchResults] = useLocalStorageState('searchResults', []);
-  const [moviesToShow, setMoviesToShow] = useState([]);
+  const [moviesToDisplay, setMoviesToDisplay] = useState([]);
   const [searchError, setSearchError] = useState('');
 
   const [isNothingFound, setIsNothingFound] = useState(false);
   const [isShortMeter, setIsShortMeter] = useLocalStorageState('isShortMeter', false);
   const [isPreloaderShown, setIsPreloaderShown] = useState(false);
 
-  const getMoviesFromApi = () => {
-    setIsPreloaderShown(true);
-    setAllMovies([]);
-    setSearchError('');
-    movieApi
-      .getMovies()
-      .then(result => {
-        const allMoviesList = result.map(movie => {
-          let isSaved = false;
-          let dbId = null;
-          savedMovies.forEach(savedMovie => {
-            if (savedMovie.movieId === movie.id) {
-              isSaved = true;
-              dbId = savedMovie._id;
-            }
-          });
-          return { ...movie, isSaved, dbId };
-        });
-        console.log(allMoviesList);
-        setAllMovies(allMoviesList);
-      })
-      .catch(error => {
-        console.log(error);
-        setSearchError(ERROR_MESSAGES.REQUEST_ERROR);
-      });
-  };
-
   const toggleIsShortMeter = event => {
     event.preventDefault();
     setIsShortMeter(!isShortMeter);
-  };
-
-  const handleSearchMovie = () => {
-    getMoviesFromApi();
-    setIsNothingFound(false);
-    const searchMoviesResults = searchMovies(searchQuery, allMovies);
-    setSearchResults(searchMoviesResults);
-    setIsPreloaderShown(false);
   };
 
   // check if something ready to be displayed in searchResults
@@ -77,12 +42,42 @@ export default function Movies({ moviesList, savedMovies }) {
         }
       });
       shortMoviesList.length === 0 ? setIsNothingFound(true) : setIsNothingFound(false);
-      setMoviesToShow(shortMoviesList);
+      setMoviesToDisplay(shortMoviesList);
     } else {
-      searchResults.length !== 0 && setIsNothingFound(false);
-
-      setMoviesToShow(searchResults);
+      searchResults.length === 0 ? setIsNothingFound(true) : setIsNothingFound(false);
+      setMoviesToDisplay(searchResults);
     }
+  };
+
+  const handleSearchMovie = () => {
+    setIsNothingFound(false);
+    setIsPreloaderShown(true);
+    setAllMovies([]);
+    setSearchError('');
+    movieApi
+      .getMovies()
+      .then(result => {
+        const allMoviesList = result.map(movie => {
+          let isSaved = false;
+          let dbId = null;
+          savedMovies.forEach(savedMovie => {
+            if (savedMovie.movieId === movie.id) {
+              isSaved = true;
+              dbId = savedMovie._id;
+            }
+          });
+          return { ...movie, isSaved, dbId };
+        });
+        // console.log(allMoviesList);
+        setAllMovies(allMoviesList);
+        const searchMoviesResults = searchMovies(searchQuery, allMovies);
+        setSearchResults(searchMoviesResults);
+        setIsPreloaderShown(false);
+      })
+      .catch(error => {
+        console.log(error);
+        setSearchError(ERROR_MESSAGES.REQUEST_ERROR);
+      });
   };
 
   return (
@@ -103,8 +98,8 @@ export default function Movies({ moviesList, savedMovies }) {
               <p className="movies__nothing-found">{ERROR_MESSAGES.NOTHING_FOUND}</p>
             )}
             {searchError && <p className="movies__search-error">{ERROR_MESSAGES.REQUEST_ERROR}</p>}
-            {moviesToShow.length > 0 && (
-              <MoviesCardList moviesList={moviesToShow} keyFieldName="id" />
+            {moviesToDisplay.length > 0 && (
+              <MoviesCardList moviesList={moviesToDisplay} keyFieldName="id" />
             )}
           </>
         )}
