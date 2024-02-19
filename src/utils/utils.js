fd8c5744565e8bb42945e8cd01e4d15ae0f2e3a0
Ivan Lev <fanatos@mail.ru>
2024-02-19 18:25:29 +0500

    refactor: update util which cleans movies titles and queries from garbage

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,25 +1,20 @@
 const cleanString = string => {
-  return string
-    .replace(/\s\s+/g, ' ') // replace multiple spaces with singl ones
-    .replace(/[^a-zA-Zа-яА-Я' ]/g, '') // clear words from special characters
-    .toLowerCase() // lowercase the phrase
-    .split(' ') // split string to array of words
-    .filter(word => {
-      // remove empty elements and short words
-      if (word.length > 2) {
-        return word;
-      }
-    });
+  return (
+    string
+      .replace(/\s\s+/g, ' ') // replace multiple spaces with singl ones
+      .replace(/[^a-zA-Zа-яА-Я' ]/g, '') // clear words from special characters
+      .toLowerCase() // lowercase the phrase
+      .split(' ') // split string to array of words
+      // remove empty elements and words shorter than 2 letters
+      .filter(word => word.length > 2)
+  );
 };
 
 export const filterMovies = (searchQuery, movieList) => {
   const filteredByQueryMovies = movieList.filter(movie => {
     const searchQueryWords = cleanString(searchQuery);
-    const movieTitleWords = [];
-    movieTitleWords.push(...cleanString(movie.nameRU), ...cleanString(movie.nameEN));
-    if (movieTitleWords.some(word => searchQueryWords.includes(word))) {
-      return movie;
-    }
+    const movieTitleWords = [...cleanString(movie.nameRU), ...cleanString(movie.nameEN)];
+    return movieTitleWords.some(word => searchQueryWords.includes(word));
   });
   return filteredByQueryMovies;
 };
